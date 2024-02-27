2d9ded947139577782d18368121b9d3025dd7811
paschyz <paschy2002@gmail.com>
2024-02-27 02:37:51 +0100

    added default values for configuration variables worker and include config.toml

--- worker/src/bin/main.rs
+++ worker/src/bin/main.rs
@@ -1,11 +1,9 @@
 use clap::Parser;
 use log::{debug, error, info, trace, warn, LevelFilter};
 use std::{
-    io::{ErrorKind, Read, Write},
-    net::{TcpStream},
-    thread,
+    env, fs, io::{ErrorKind, Read, Write}, net::TcpStream, thread
 };
-
+use toml::Value;
 use dotenv::dotenv;
 
 extern crate worker;
@@ -24,37 +22,64 @@ use worker::{
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(long, default_value_t = HOST.to_string())]
-    host: String,
+    #[arg(long)]
+    host: Option<String>,
 
-    #[arg(long, default_value_t = *PORT)]
-    port: u16,
+    #[arg(long)]
+    port: Option<String>,
 
     #[arg(long)]
     name: Option<String>,
 
-    #[arg(long, default_value_t = RUST_ENV.to_string())]
-    rust_env: String,
+    #[arg(long)]
+    rust_env: Option<String>,
 }
+
 
 fn main() {
     dotenv().ok();
 
     local_env::check_vars();
     let args = Args::parse();
+    let contents = fs::read_to_string("worker/Config.toml").expect("Unable to read file");
 
-    let name = args
-        .name
-        .unwrap_or_else(|| shared::utils::random_string(10));
+    let parsed_toml: Value = contents.parse().expect("Unable to parse TOML");
 
-    logger::setup_logger(&args.rust_env.as_str());
+
+    let host = args.host
+    .or(parsed_toml.get("host").and_then(|v| v.as_str()).map(String::from))
+    .or_else(|| env::var("HOST").ok())
+    .unwrap_or_else(|| "random".to_string());
+
+    let port = args.port
+    .or(parsed_toml.get("port").and_then(|v| v.as_str()).map(String::from))
+    .or_else(|| env::var("port").ok())
+    .unwrap_or_else(|| "random".to_string());
+
+    let name = args.name
+    .or(parsed_toml.get("name").and_then(|v| v.as_str()).map(String::from))
+    .or_else(|| env::var("NAME").ok())
+    .unwrap_or_else(|| shared::utils::random_string(10));
+
+    let rust_env = args.rust_env
+    .or(parsed_toml.get("rust_env").and_then(|v| v.as_str()).map(String::from))
+    .or_else(|| env::var("RUST_ENV").ok())
+    .unwrap_or_else(|| "random".to_string());
+
+    println!("Host: {:?}", host);
+    println!("Port: {:?}", port);
+    println!("Name: {:?}", name);
+    println!("rust_env: {:?}", rust_env);
+    
+  
+    logger::setup_logger(rust_env.as_str());
 
     info!("Worker {} ok", name);
 
     loop {
         thread::sleep(std::time::Duration::from_secs(1));
 
-        let addr = match network::get_socket_addr(&args.host.as_str(), args.port) {
+        let addr = match network::get_socket_addr(host.as_str(), port.parse::<u16>().unwrap()) {
             Ok(addr) => addr,
             Err(e) => {
                 error!("Failed to parse address: {}", e);
